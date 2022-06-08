2b221f644ca74fb16b258061aa486a476b5c4254
Michael Ruhl <michaelruhl14@hotmail.com>
2022-06-08 17:25:13 +0000

    Use if else to match correct school with age

---
+++ whichSchool.js
@@ -0,0 +1,9 @@
+const age = 12
+
+if ( age < 13) {
+  console.log('Elementary School');
+}
+else if ((age >= 13) && (age <= 18)) {
+  console.log('High School');
+}
+else {console.log('Lighthouse Labs');}
